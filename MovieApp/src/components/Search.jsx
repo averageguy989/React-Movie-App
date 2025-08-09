273c694b7ba41ac700a03d46f8d6562e5fd65899
averageguy989 <chandrucp678@gmail.com>
2025-08-10 00:12:26 +0530

    Added movies fetch functionality, error displaying, and loading bar

--- MovieApp/src/components/Search.jsx
+++ MovieApp/src/components/Search.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-function Search() {
+function Search({searchTerm,setSearchTerm}) {
   return (
-    <div className='text-white text-3xl'>
-      Search
+    <div className='search'>
+      <div>
+        <img src="search.svg" alt='search logo'/>
+        <input
+          type='text'
+          placeholder='Search through thousands of movies'
+          value={searchTerm}
+          onChange={(e)=>setSearchTerm(e.target.value)}
+        />
+      </div>
     </div>
   )
 }
